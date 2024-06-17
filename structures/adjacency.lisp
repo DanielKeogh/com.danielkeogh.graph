9bc0ed043e82f4507078f4b308bc779570f32a59
Daniel Keogh <keogh.daniel@gmail.com>
2024-06-17 12:33:13 +1000

    Adding optimizing declarations to adjacency graph.

--- structures/adjacency.lisp
+++ structures/adjacency.lisp
@@ -57,12 +57,14 @@
 (defun make-graph (&key
                      (allow-parallel-edges t)
                      (vertex-equality-fn #'eql))
+  (declare #.utils:*internal-optimize-settings*)
   (make-adjacency-graph
    :allow-parallel-edges allow-parallel-edges
    :vertex-edges (make-hash-table :test vertex-equality-fn)
    :vertex-equality-fn vertex-equality-fn))
 
 (defun make-edge-list ()
+  (declare #.utils:*internal-optimize-settings*)
   (list))
 
 ;; macros
@@ -74,6 +76,9 @@
 ;; utils
 
 (defun edge-equal (graph edge source target)
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type edge:edge edge))
   (let ((test (graph-vertex-equality-fn graph)))
     (and (funcall test source (edge:edge-source edge))
          (funcall test target (edge:edge-target edge)))))
@@ -81,30 +86,39 @@
 ;; api
 
 (defun has-vertex (graph vertex)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (when (nth-value 1 (gethash vertex (graph-vertex-edges graph)))
     t))
 
 (defun has-edge (graph edge)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
-  (a:when-let (edges (gethash (edge:edge-source edge) (graph-vertex-edges graph)))
-    (find edge edges :test #'eql)))
+  (declare (type edge:edge edge))
+  (let ((edges (gethash (edge:edge-source edge) (graph-vertex-edges graph))))
+    (declare (type (or null list) edges))
+    (when edges
+      (find edge edges :test #'eql))))
 
 (defun has-edge-between (graph source target)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
-  (a:when-let (edges (gethash source (graph-vertex-edges graph)))
-    (loop for edge in edges
-            thereis (edge-equal graph edge source target))))
+  (let ((edges (gethash source (graph-vertex-edges graph))))
+    (declare (type (or null list) edges))
+    (when edges
+      (loop for edge in edges
+              thereis (edge-equal graph edge source target)))))
 
 (defun add-vertex (graph vertex)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (unless (has-vertex graph vertex)
     (setf (gethash vertex (graph-vertex-edges graph)) (make-edge-list))))
 
 (defun %add-edge (graph edge)
-  (declare (type adjacency-graph graph)
-           (type edge:edge edge)
-           (optimize (speed 3) (safety 0)))
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type edge:edge edge))
   (let ((source (edge:edge-source edge))
         (target (edge:edge-target edge)))
     (ensure-vertex graph source)
@@ -115,17 +129,20 @@
       edge)))
 
 (defun add-edge (graph edge)
-  (declare (type adjacency-graph graph)
-           (type edge:edge edge))
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type edge:edge edge))
   (%add-edge graph edge))
 
 (defun add-edge-between (graph source target)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (%add-edge graph (edge:make-edge source target)))
 
 (defun remove-edge (graph edge)
-  (declare (type adjacency-graph graph)
-           (type edge:edge edge))
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type edge:edge edge))
   (edge:with-edge (source target) edge
     (ensure-vertex graph source)
     (ensure-vertex graph target)
@@ -133,6 +150,7 @@
           (remove edge (gethash source (graph-vertex-edges graph))))))
 
 (defun remove-edge-between (graph source target)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (ensure-vertex graph source)
   (ensure-vertex graph target)
@@ -141,8 +159,9 @@
           (remove-if should-remove-edge (gethash source (graph-vertex-edges graph))))))
 
 (defun remove-in-edges (graph filter-fn)
-  (declare (type adjacency-graph graph)
-           (type function filter-fn))
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type function filter-fn))
   (let ((vertex-hash (graph-vertex-edges graph)))
     (with-hash-table-iterator (itr vertex-hash)
       (loop
@@ -152,6 +171,7 @@
           (setf (gethash key vertex-hash) (remove-if filter-fn val)))))))
 
 (defun remove-vertex (graph vertex)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (let ((eq-fn (graph-vertex-equality-fn graph)))
     (remhash vertex (graph-vertex-edges graph))
@@ -160,20 +180,24 @@
 ;; utils
 
 (defun edges (graph)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (loop for edge-collection being the hash-values of (graph-vertex-edges graph)
         nconc (copy-list edge-collection)))
 
 (defun vertices (graph)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (loop for vertex being the hash-keys of (graph-vertex-edges graph)
         collect vertex))
 
 (defun out-edges (graph vertex)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (gethash vertex (graph-vertex-edges graph)))
 
 (defun in-edges (graph vertex)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (let ((vertex-hash (graph-vertex-edges graph))
         (vertex-eq (graph-vertex-equality-fn graph)))
@@ -184,31 +208,36 @@
                  val))))
 
 (defun vertex-count (graph)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
   (hash-table-count (graph-vertex-edges graph)))
 
 (defun edge-count (graph)
+  (declare #.utils:*internal-optimize-settings*)
   (declare (type adjacency-graph graph))
-  (loop for edge-collection being the hash-values of (graph-vertex-edges graph)
+  (loop for edge-collection of-type list being the hash-values of (graph-vertex-edges graph)
         sum (length edge-collection)))
 
 ;; looping without malloc
 
 (defun for-edges (graph fn)
-  (declare (type adjacency-graph graph)
-           (type function fn))
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type function fn))
   (loop for edge-collection being the hash-values of (graph-vertex-edges graph)
-        do (loop for edge in edge-collection
-                 do (funcall fn edge))))
+        do (dolist (edge edge-collection)
+             (funcall fn edge))))
 
 (defun for-vertices (graph fn)
-  (declare (type adjacency-graph graph)
-           (type function fn))
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type function fn))
   (loop for vertex being the hash-keys of (graph-vertex-edges graph)
         do (funcall fn vertex)))
 
 (defun for-out-edges (graph vertex fn)
-  (declare (type adjacency-graph graph)
-           (type function fn))
-  (loop for edge in (out-edges graph vertex)
-        do (funcall fn edge)))
+  (declare #.utils:*internal-optimize-settings*)
+  (declare (type adjacency-graph graph))
+  (declare (type function fn))
+  (dolist (edge (out-edges graph vertex))
+    (funcall fn edge)))
